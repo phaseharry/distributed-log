4342915c4598a2a93d9147961bc79d873d3576b4
phaseharry <harrychenhc@icloud.com>
2025-10-04 13:01:52 -0400

    add segment tests

---
+++ write-a-log-package/internal/log/segment_test.go
@@ -0,0 +1,64 @@
+package log
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	api "github.com/phaseharry/distributed-log/write-a-log-package/api/v1"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSegment(t *testing.T) {
+	dir, _ := ioutil.TempDir("", "segment-test")
+	defer os.RemoveAll(dir)
+
+	want := &api.Record{Value: []byte("hello world")}
+
+	c := Config{}
+	c.Segment.MaxStoreBytes = 1024
+	c.Segment.InitialOffset = entWidth * 3
+
+	s, err := newSegment(dir, 16, c)
+	require.NoError(t, err)
+	require.Equal(t, uint64(16), s.nextOffset, s.nextOffset)
+	require.False(t, s.IsMaxed())
+
+	// testing that we can append records to a segment and read it back
+	for i := uint64(0); i < 3; i++ {
+		off, err := s.Append(want)
+		require.NoError(t, err)
+		require.Equal(t, 16+i, off)
+
+		got, err := s.Read(off)
+		require.NoError(t, err)
+		require.Equal(t, want.Value, got.Value)
+	}
+	_, err = s.Append(want)
+	require.Equal(t, io.EOF, err)
+
+	// testing that we've reached max size on the segment
+	require.True(t, s.IsMaxed())
+
+	c.Segment.MaxStoreBytes = uint64(len(want.Value) * 3)
+	c.Segment.MaxIndexBytes = 1024
+
+	s, err = newSegment(dir, 16, c)
+	require.NoError(t, err)
+	/*
+	   creating a new segment with the same store and index files to confirm
+	   that the segment would still be maxed
+	*/
+	require.True(t, s.IsMaxed())
+	err = s.Remove()
+	require.NoError(t, err)
+
+	/*
+		removing the segment and creating a new one with same directory name and confirming
+		that it is not empty anymore
+	*/
+	s, err = newSegment(dir, 16, c)
+	require.NoError(t, err)
+	require.False(t, s.IsMaxed())
+}
